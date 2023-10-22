82f3c859674e80cbd7fb3c2d3614ac7ca94c2a51
Vitor Faiante <vitorf7@gmail.com>
2023-10-22 19:48:11 +0100

    refactor: move to using lazyvim extras

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -8,24 +8,24 @@ return {
   { import = "plugins" },
 
   -- Coding
-  { import = "lazyvim.plugins.extras.coding.codeium" },
-
-  -- DAP
-  { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.dap.nlua" },
-
-  -- Languages
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.terraform" },
-
-  -- Neotest
-  { import = "lazyvim.plugins.extras.test.core" },
-
-  -- UI
-  { import = "lazyvim.plugins.extras.ui.edgy" },
-
-  -- Projects
-  { import = "lazyvim.plugins.extras.util.project" },
+  -- { import = "lazyvim.plugins.extras.coding.codeium" },
+  --
+  -- -- DAP
+  -- { import = "lazyvim.plugins.extras.dap.core" },
+  -- { import = "lazyvim.plugins.extras.dap.nlua" },
+  --
+  -- -- Languages
+  -- { import = "lazyvim.plugins.extras.lang.typescript" },
+  -- { import = "lazyvim.plugins.extras.lang.json" },
+  -- { import = "lazyvim.plugins.extras.lang.yaml" },
+  -- { import = "lazyvim.plugins.extras.lang.terraform" },
+  --
+  -- -- Neotest
+  -- { import = "lazyvim.plugins.extras.test.core" },
+  --
+  -- -- UI
+  -- { import = "lazyvim.plugins.extras.ui.edgy" },
+  --
+  -- -- Projects
+  -- { import = "lazyvim.plugins.extras.util.project" },
 }
