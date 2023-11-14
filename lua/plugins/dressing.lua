5384abe44dc09069d0f23e2289b218d26e5b0f82
Vitor Faiante <vfaiante@uw.co.uk>
2023-11-14 13:21:20 +0000

    feat: add smart open and legendary

---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/dressing.nvim",
+  opts = {
+    select = {
+      get_config = function(opts)
+        if opts.kind == "legendary.nvim" then
+          return {
+            telescope = {
+              sorter = require("telescope.sorters").fuzzy_with_index_bias({}),
+            },
+          }
+        else
+          return {}
+        end
+      end,
+    },
+  },
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,21 @@
 return {
   "telescope.nvim",
   dependencies = {
+    "kkharji/sqlite.lua",
     "rcarriga/nvim-notify",
     "ThePrimeagen/harpoon",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
+    },
+    {
+      "danielfalk/smart-open.nvim",
+      branch = "0.2.x",
+      config = function() end,
+      dependencies = {
+        "kkharji/sqlite.lua",
+        { "nvim-telescope/telescope-fzy-native.nvim" },
+      },
     },
   },
   opts = {
@@ -16,6 +26,70 @@ return {
           ["<C-k>"] = require("telescope.actions").move_selection_previous,
         },
       },
+      file_ignore_patterns = { ".git/", "node_modules" },
+      layout_config = {
+        height = 0.90,
+        width = 0.90,
+        preview_cutoff = 0,
+        horizontal = { preview_width = 0.60 },
+        vertical = { width = 0.55, height = 0.9, preview_cutoff = 0 },
+        prompt_position = "top",
+      },
+      path_display = { "smart" },
+      prompt_position = "top",
+      prompt_prefix = " ",
+      selection_caret = " ",
+      sorting_strategy = "ascending",
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--hidden",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--trim", -- add this value
+      },
+    },
+    pickers = {
+      buffers = {
+        prompt_prefix = "󰸩 ",
+      },
+      commands = {
+        prompt_prefix = " ",
+        layout_config = {
+          height = 0.63,
+          width = 0.78,
+        },
+      },
+      command_history = {
+        prompt_prefix = " ",
+        layout_config = {
+          height = 0.63,
+          width = 0.58,
+        },
+      },
+      git_files = {
+        prompt_prefix = "󰊢 ",
+        show_untracked = true,
+      },
+      find_files = {
+        prompt_prefix = " ",
+        find_command = { "fd", "-H" },
+      },
+      live_grep = {
+        prompt_prefix = "󰱽 ",
+      },
+      grep_string = {
+        prompt_prefix = "󰱽 ",
+      },
+    },
+    extensions = {
+      smart_open = {
+        -- cwd_only = true,
+        filename_first = true,
+      },
     },
   },
   config = function(_, opts)
@@ -24,5 +98,6 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("harpoon")
     telescope.load_extension("notify")
+    telescope.load_extension("smart_open")
   end,
 }
