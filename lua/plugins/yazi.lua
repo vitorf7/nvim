caf3cba576d29975c4425efaaddae4f8e17266a6
Vitor Faiante <vfaiante@uw.co.uk>
2023-11-14 13:19:39 +0000

    refactor: swap lf for yazi

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,11 +1,12 @@
 return {
   "DreamMaoMao/yazi.nvim",
+  lazy = false,
   dependencies = {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
   },
 
   keys = {
-    { "<leader>gy", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+    { "<leader>fy", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
   },
 }
