7fde63e61bf6e8c233780764826db87e450146d9
Vitor Faiante <vitorf7@gmail.com>
2024-01-03 13:28:25 +0000

    chore: remove legendary

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,18 +1,1 @@
-return {
-  "stevearc/dressing.nvim",
-  opts = {
-    select = {
-      get_config = function(opts)
-        if opts.kind == "legendary.nvim" then
-          return {
-            telescope = {
-              sorter = require("telescope.sorters").fuzzy_with_index_bias({}),
-            },
-          }
-        else
-          return {}
-        end
-      end,
-    },
-  },
-}
+return {}
