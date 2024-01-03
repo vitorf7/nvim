36caabd098fd2a065a8cd4969b087e7ea5b0a4a3
Vitor Faiante <vitorf7@gmail.com>
2024-01-03 13:27:57 +0000

    refactor: change theme

---
+++ lua/plugins/fluoromachine.lua
@@ -0,0 +1,14 @@
+return {
+  "maxmx03/fluoromachine.nvim",
+  config = function()
+    local fm = require("fluoromachine")
+
+    fm.setup({
+      glow = false,
+      transparent = "full",
+      theme = "fluoromachine",
+    })
+
+    vim.cmd.colorscheme("fluoromachine")
+  end,
+}
