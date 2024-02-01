7a7b2c046d11bb64f23ca41523c4bd6c05bc372d
Vitor Faiante <vitorf7@gmail.com>
2024-02-01 15:23:14 +0000

    refactor: use harpoon 2
    use harpoon 2 but still need to actually learn to use it properly

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,8 +1,13 @@
 return {
   "ThePrimeagen/harpoon",
-  dependencies = "nvim-lua/plenary.nvim",
-  opts = {
-    global_settings = { mark_branch = true },
-    width = vim.api.nvim_win_get_width(0) - 4,
-  },
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  -- opts = {
+  --   global_settings = { mark_branch = true },
+  --   width = vim.api.nvim_win_get_width(0) - 4,
+  -- },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+  end,
 }
