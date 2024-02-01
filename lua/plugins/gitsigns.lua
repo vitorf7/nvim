7ae67e4b5cf9028f475e07aec0d7168c775e944e
Vitor Faiante <vitorf7@gmail.com>
2024-02-01 15:21:42 +0000

    refactor: change gitsigns blame line date format

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,7 +13,7 @@ return {
       delay = 500,
     },
     current_line_blame = true,
-    current_line_blame_formatter = "<author>, <abbrev_sha> <author_time:%y-%m-%d> - <summary>",
+    current_line_blame_formatter = "<author>, <abbrev_sha> <author_time:%d-%m-%y> - <summary>",
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
